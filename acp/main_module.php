20ed511520ba6d4b95c68f6d8cd34c78ed63c7b5
restless-rancor <44141026+restless-rancor@users.noreply.github.com>
2018-11-01 14:28:54 +0000

    Comply with phpBB coding guidelines

--- acp/main_module.php
+++ acp/main_module.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *
- * Attachment Shields. An extension for the phpBB Forum Software package.
- *
- * @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* Attachment Shields. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
 
 namespace restlessrancor\attachmentshields\acp;
 
@@ -33,7 +33,7 @@ class main_module
 		$this->page_title = $user->lang('ASHIELDS_TITLE');
 		
 		// define form
-		$form_name = 'attachmentshields_enable';
+		$form_name = 'ashields_enable';
 		add_form_key($form_name);
 		
 		$this->config = $config;
--- event/main_listener.php
+++ event/main_listener.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *
- * Attachment Shields. An extension for the phpBB Forum Software package.
- *
- * @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* Attachment Shields. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
 
 namespace restlessrancor\attachmentshields\event;
 
@@ -31,7 +31,7 @@ class main_listener implements EventSubscriberInterface
 		$this->config = $config;
     }
 
-    public static function getSubscribedEvents()
+    static public function getSubscribedEvents()
     {
         return array(
             'core.user_setup' => 'load_language_on_setup',
